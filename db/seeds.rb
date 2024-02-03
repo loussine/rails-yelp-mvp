ffb868c10695b1ce78f139eb56c6c76e429cf87b
Yolande Touchet <yolandetouchet@hotmail.com>
2024-02-03 03:18:52 +0100

    Rails 3 - Advances Routing - Exo 3 - Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Seed restaurants
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+
+# Seed reviews
+Restaurant.all.each do |restaurant|
+  rand(1..3).times do
+    Review.create!(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant: restaurant
+    )
+  end
+end
