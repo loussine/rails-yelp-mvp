940e6a0dcd8d0339e319b33c7799e958d84f25f2
Yolande Touchet <yolandetouchet@hotmail.com>
2024-02-03 18:24:28 +0100

    Rails 3 - Advanced Routing - Exo 2 - Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # ReviewsController
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create]
+      # get 'reviews', to: 'reviews#index'
+      # get 'reviews/new', to: 'reviews#new', as: 'new_review'
+      # post 'reviews', to: 'reviews#create', as: 'create_review'
+  end
+
+  # Un visiteur peut ajouter un nouvel avis à un restaurant.
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+
+
 end
